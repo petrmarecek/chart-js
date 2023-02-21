449493034a7dd52fa55e101037992592fb264dc4
Petr Marecek <petr.marecekk@gmail.com>
2023-02-21 22:56:44 +0100

    Add comments

--- src/components/lineChart/data.ts
+++ src/components/lineChart/data.ts
@@ -1,4 +1,6 @@
 import {ChartOptions} from "chart.js";
+
+// y-axis
 const labels = ['01/2022', '02/2022', '03/2022', '04/2022', '05/2022', '06/2022', '07/2022']
 
 export const data = {
@@ -6,7 +8,7 @@ export const data = {
     datasets: [
         {
             label: '1 - 90th percentil',
-            data: [null, null, null, null, 10000, 23000, 26000],
+            data: [null, null, null, null, 10000, 23000, 26000], // x-axis
             borderColor: 'red',
             backgroundColor: 'red',
             fill: '+1',
@@ -14,14 +16,14 @@ export const data = {
         },
         {
             label: 'Forecast line',
-            data: [8000, 26000, 8000, 5000, 10000, 20000, 22000],
+            data: [8000, 26000, 8000, 5000, 10000, 20000, 22000], // x-axis
             borderColor: 'blue',
             backgroundColor: 'blue',
             tension: 0.2,
         },
         {
             label: '2 - 90th percentil',
-            data: [null, null, null, null, 10000, 17000, 18000],
+            data: [null, null, null, null, 10000, 17000, 18000], // x-axis
             borderColor: 'red',
             backgroundColor: 'red',
             fill: '-1',
@@ -39,12 +41,7 @@ export const options: ChartOptions<'line'> = {
     },
     scales: {
         x: {
-            display: true,
-            title: {
-                display: true,
-                text: "Month",
-            },
-            ticks: {
+            ticks: { // possible to change step for x-axis or y-axis
                 callback: function(value, index) {
                     return labels[index];
                 },
@@ -52,14 +49,14 @@ export const options: ChartOptions<'line'> = {
         },
         y: {
             display: true,
-            min: 0,
-            max: 30000,
+            min: 0, // y-range
+            max: 30000, // y-range
             title: {
                 display: true,
-                text: "Cost",
+                text: "Cost", // y-label
             },
             ticks: {
-                stepSize: 5000
+                stepSize: 5000 // y-step
             }
         }
     }
